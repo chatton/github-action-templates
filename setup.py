c445407056533af8ff7b1dff3b72d4e0b91d43e1
chatton <cianhatton@gmail.com>
2021-06-18 23:22:53 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(
+    name="GithubActionTemplates",
+    version="0.0.1",
+    description="Template GitHub Actions",
+    author="Cian Hatton",
+    author_email="cianhatton@gmail.com",
+    url="https://github.com/chatton/GithubActionTemplates",
+    packages=find_packages(),
+    scripts=["template.py"],
+)
--- template.py
+++ template.py
@@ -3,9 +3,14 @@
 import sys
 import argparse
 from typing import Dict, List
-
+import os
 import ruamel.yaml
+
 yaml = ruamel.yaml.YAML()
+
+DEFAULT_JOBS_DIR = "action_templates/jobs"
+DEFAULT_STEPS_DIR = "action_templates/steps"
+DEFAULT_EVENTS_DIR = "action_templates/events"
 
 
 def _parse_args() -> argparse.Namespace:
@@ -19,13 +24,31 @@ def _load_template_file(filename: str) -> Dict:
         return yaml.load(f.read())
 
 
+def _get_file_path_or_default(path: str, default_path: str) -> str:
+    if os.path.exists(path):
+        return path
+    if os.path.exists(path+".yml"):
+        return path+".yml"
+    if os.path.exists(path+".yaml"):
+        return path+".yaml"
+
+    default = os.path.join(default_path, path)
+    if os.path.exists(default):
+        return default
+    if os.path.exists(default+".yml"):
+        return default+".yml"
+    if os.path.exists(default+".yaml"):
+        return default+".yaml"
+    raise ValueError("No such file: {}!".format(path))
+
+
 def _load_jobs(template: Dict) -> List:
     job_dicts = []
     jobs = template["jobs"]
 
     for job in jobs:
         path = job["path"]
-        with open(path) as f:
+        with open(_get_file_path_or_default(path, DEFAULT_JOBS_DIR)) as f:
 
             job_dict = yaml.load(f.read())
 
@@ -46,7 +69,7 @@ def _get_states(job: Dict) -> List[str]:
     final_steps = []
     steps = job["steps"]
     for step in steps:
-        with open(step, "r") as sf:
+        with open(_get_file_path_or_default(step, DEFAULT_STEPS_DIR), "r") as sf:
             step_list = yaml.load(sf.read())
             for s in step_list:
                 final_steps.append(s)
@@ -57,7 +80,7 @@ def _load_events(template: Dict) -> Dict:
     events = []
     event_filepaths = template["events"]
     for filepath in event_filepaths:
-        with open(filepath) as f:
+        with open(_get_file_path_or_default(filepath, DEFAULT_EVENTS_DIR)) as f:
             events.append(yaml.load(f.read()))
     on_obj = {}
     for e in events:
