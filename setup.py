7514ba101a93d2aed2775435420e33d5e937b0b1
chatton <cianhatton@gmail.com>
2021-06-18 23:34:51 +0100

    Bumped to v0.0.2

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="GithubActionTemplates",
-    version="0.0.1",
+    version="0.0.2",
     description="Template GitHub Actions",
     author="Cian Hatton",
     author_email="cianhatton@gmail.com",
     url="https://github.com/chatton/GithubActionTemplates",
     packages=find_packages(),
-    scripts=["template.py"],
 )
