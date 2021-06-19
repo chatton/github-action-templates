95ff774abe449101872403e783330313600bb360
chatton <cianhatton@gmail.com>
2021-06-19 18:51:04 +0100

    Fixed logic creating jobs

--- ghat/template.py
+++ ghat/template.py
@@ -3,9 +3,14 @@ import os
 import ruamel.yaml
 yaml = ruamel.yaml.YAML()
 
-DEFAULT_JOBS_DIR = "action_templates/jobs"
-DEFAULT_STEPS_DIR = "action_templates/steps"
-DEFAULT_EVENTS_DIR = "action_templates/events"
+DEFAULT_JOBS_DIR = ".action_templates/jobs"
+DEFAULT_STEPS_DIR = ".action_templates/steps"
+DEFAULT_EVENTS_DIR = ".action_templates/events"
+
+
+def pp(obj):
+    import json
+    print(json.dumps(obj, indent=4))
 
 
 def _load_template_file(filename: str) -> Dict:
@@ -32,28 +37,24 @@ def _get_file_path_or_default(path: str, default_path: str) -> str:
 
 
 def _load_jobs(template: Dict) -> Dict:
-    job_list = []
-    jobs = template["jobs"]
-
-    for job in jobs:
-        path = job["template"]
+    job_templates = template["jobs"]
+    final_jobs = {}
+    for job_template in job_templates:
+        path = job_template["template"]
         with open(_get_file_path_or_default(path, DEFAULT_JOBS_DIR)) as f:
             job_dict = yaml.load(f.read())
-            # specified a condition in the template file.
-            if "if" in job:
-                job_dict["if"] = job["if"]
+            for job_name in job_dict:
+                # we move any "if" specified in the template to all of the jobs from the templates.
+                if "if" in job_template:
+                    job_dict[job_name]["if"] = job_template["if"]
 
-            # specified steps in template file.
-            if "steps" in job:
-                job_dict["steps"] = _get_steps(job)
+                # we assign directly all of the steps references in the templates.
+                if "steps" in job_template:
+                    job_dict[job_name]["steps"] = _get_steps(job_template)
 
-            job_list.append(job_dict)
-
-    on_obj = {}
-    for e in job_list:
-        for k, v in e.items():
-            on_obj[k] = v
-    return on_obj
+            # add the job by name to the final result.
+            final_jobs[job_name] = job_dict[job_name]
+    return final_jobs
 
 
 def _get_steps(job: Dict) -> List[str]:
