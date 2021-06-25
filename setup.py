c6acbc74f6114c0a5b94a76fed630b7ce32b1c1b
chatton <cianhatton@gmail.com>
2021-06-25 23:02:47 +0100

    bump version to 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="github-action-templates",
-    version="0.0.2",
+    version="0.0.3",
     description="Template GitHub Actions",
     author="Cian Hatton",
     author_email="cianhatton@gmail.com",
