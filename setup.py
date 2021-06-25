007101ddd49510e3e90a3be951fe7a1a2bc1f712
chatton <cianhatton@gmail.com>
2021-06-25 20:28:41 +0100

    added README as long description

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="GithubActionTemplates",
@@ -10,4 +15,6 @@ setup(
     author_email="cianhatton@gmail.com",
     url="https://github.com/chatton/GithubActionTemplates",
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
